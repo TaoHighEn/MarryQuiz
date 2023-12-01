73b0efa4d822705e7e5d60ba9e252d693f04b427
Alan <32477198+TaoHighEn@users.noreply.github.com>
2023-12-01 16:08:00 +0800

    加入專案檔案。

---
+++ MarryQuiz/Quiz/Quiz.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Quiz.aspx.cs" Inherits="MarryQuiz.Quiz.Quiz" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" style="">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <label>TTTTT</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                     <label>TTfdsghdfTTT</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                     <label>TTTddffdTT</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                     <label>gggg</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                </div>
+            </div>
+        </div>
+        <asp:Button ID="btn_submit" runat="server" Text="送出" OnClick="btnSubmit_Click" />
+    </form>
+</body>
+</html>
---
+++ MarryQuiz/Quiz/Quiz.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MarryQuiz.Quiz
+{
+    public partial class Quiz : System.Web.UI.Page
+    {
+        private string[,] questionsAndAnswers = 
+        {
+            { "1. 新郎、新娘是什麼時期認識的？", "大學" },
+            { "2. 新郎、新娘登記日期是幾月幾號？", "10/28" },
+            { "3. 新郎、新娘交往了幾年？", "8" },
+            { "4. 新郎、新娘第一次見面的縣市？", "雲林" },
+            { "5. 新郎、新娘的蜜月國家？", "日本" }
+        };
+        private string[,] answerarr = { { "大學", "高中", "碩士", "當兵" }, { "11/11", "10/28", "04/17", "09/20" }, { "5", "7", "8", "10" }, { "高雄", "雲林", "桃園", "台北" }, { "日本", "德國", "美國", "俄羅斯" } };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadQuestions();
+            }
+        }
+        protected void LoadQuestions()
+        {
+            for (int i = 0; i < questionsAndAnswers.Length; i++)
+            {
+                var pair = questionsAndAnswers[i,0];
+                //問題label
+                Label lblQuestion = new Label();
+                lblQuestion.Text = pair[0];
+                //回答RadioButton
+                RadioButtonList radioButtonList = new RadioButtonList();
+                radioButtonList.Items.Add("ttt");
+                radioButtonList.ID = "ans_" + pair.Key.GetHashCode(); // Generate unique ID for each answer textbox
+
+                form1.Controls.Add(lblQuestion);
+                form1.Controls.Add(new LiteralControl("<br/>"));
+                form1.Controls.Add(radioButtonList);
+                form1.Controls.Add(new LiteralControl("<br/><br/>"));
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            foreach (var pair in questionsAndAnswers)
+            {
+                string answerID = "txtAnswer_" + pair.Key.GetHashCode();
+                TextBox txtAnswer = form1.FindControl(answerID) as TextBox;
+
+                if (txtAnswer != null && txtAnswer.Text.Trim().Equals(pair.Value, StringComparison.OrdinalIgnoreCase))
+                {
+                    // Correct answer
+                    // Perform actions (e.g., add points, display correct answer message)
+                    Response.Write($"Question: {pair.Key}<br/>Your answer: {txtAnswer.Text}<br/>Correct answer: {pair.Value}<br/><br/>");
+                }
+                else if (txtAnswer != null)
+                {
+                    // Incorrect answer
+                    // Perform actions (e.g., display incorrect answer message)
+                    Response.Write($"Question: {pair.Key}<br/>Your answer: {txtAnswer.Text}<br/>Correct answer: {pair.Value}<br/><br/>");
+                }
+            }
+        }
+    }
+}
